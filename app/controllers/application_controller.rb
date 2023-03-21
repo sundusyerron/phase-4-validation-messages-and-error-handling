fa7cc2fac09cd80ac032d01341cd7fdfe391272e
Sundus <sundusaden105@gmail.com>
2023-03-21 10:42:41 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    def create
+        movie = Movie.create!(movie_params)
+        render json: movie, status: :created
+      rescue ActiveRecord::RecordInvalid => e
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+      end
 end
